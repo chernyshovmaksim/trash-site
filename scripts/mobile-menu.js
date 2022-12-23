bf4cd645b761f2e204fc90af43c9296372f6feb9
chernyshovmaksim <chernyshovmaksim56@gmail.com>
2022-12-23 21:00:29 +0500

    Changes to be committed: 	modified:   index.html 	new file:   public/favicon.svg 	modified:   scripts/mobile-menu.js

--- scripts/mobile-menu.js
+++ scripts/mobile-menu.js
@@ -1,12 +1,15 @@
 export class Hamburger {
+	#el;
+
 	constructor(el) {
 		console.log("Hamburger plugin init");
+		this.#el = el;
 		el.addEventListener("click", () => {
-			el.classList.toggle("hamburger--active");
-			this.#store();
+			this.#changeStatus();
+			this.#checkStatus();
 		});
 	}
-	#store() {
+	#changeStatus() {
 		if (localStorage.getItem("statusMM")) {
 			if (localStorage.getItem("statusMM") == "0") {
 				localStorage.setItem("statusMM", "1");
@@ -15,6 +18,17 @@ export class Hamburger {
 			}
 		} else {
 			localStorage.setItem("statusMM", "1");
+		}
+	}
+	#checkStatus() {
+		if (localStorage.getItem("statusMM")) {
+			const status = localStorage.getItem("statusMM");
+
+			if (status == "0") {
+				this.#el.classList.remove("hamburger--active");
+			} else {
+				this.#el.classList.add("hamburger--active");
+			}
 		}
 	}
 }
