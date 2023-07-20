de231e0570031606f5113febf09373beed8d56d4
Max Chernyshov <chernyshovmaksim56@gmail.com>
2023-07-20 14:28:12 +0500

    add about block to about page

--- main.js
+++ main.js
@@ -2,9 +2,6 @@
 import "./scss/main.scss";
 import "./public/icon-fonts.css";
 
-//import utils
-import "./scripts/pxpe";
-
 //import scripts
 import "./scripts/top-slider";
 import "./scripts/reviews";
@@ -13,36 +10,36 @@ import { Hamburger } from "./scripts/mobile-menu";
 import { Quiz } from "./scripts/quiz";
 
 document.addEventListener("DOMContentLoaded", () => {
-	//Mobile menu
-	if (document.querySelector(".hamburger")) {
-		const hamburgerEl = document.querySelector(".hamburger");
-		const siteNameEl = document.querySelector(".site-name");
-		new Hamburger(hamburgerEl, siteNameEl);
-	}
+  //Mobile menu
+  if (document.querySelector(".hamburger")) {
+    const hamburgerEl = document.querySelector(".hamburger");
+    const siteNameEl = document.querySelector(".site-name");
+    new Hamburger(hamburgerEl, siteNameEl);
+  }
 
-	//Form, input phone
-	const inputsTel = document.querySelectorAll("input[name=phone]");
-	inputsTel.forEach((el) => {
-		inputPhone(el);
-	});
+  //Form, input phone
+  const inputsTel = document.querySelectorAll("input[name=phone]");
+  inputsTel.forEach((el) => {
+    inputPhone(el);
+  });
 
-	//Advantages
-	if (document.querySelector(".advantages-mobile")) {
-		document.querySelectorAll(".advantages-mobile").forEach((el) => {
-			el.addEventListener("click", (e) => {
-				//console.log(e.target.classList);
-				if (
-					e.target.classList.contains("advantage-mobile__bodyHead") ||
-					e.target.classList.contains("advantage-mobile__carret") ||
-					e.target.classList.contains("icon-carret") ||
-					e.target.classList.contains("advantage-mobile__preview-icons")
-				) {
-					el.classList.toggle("advantages-mobile--active");
-				}
-			});
-		});
-	}
+  //Advantages
+  if (document.querySelector(".advantages-mobile")) {
+    document.querySelectorAll(".advantages-mobile").forEach((el) => {
+      el.addEventListener("click", (e) => {
+        //console.log(e.target.classList);
+        if (
+          e.target.classList.contains("advantage-mobile__bodyHead") ||
+          e.target.classList.contains("advantage-mobile__carret") ||
+          e.target.classList.contains("icon-carret") ||
+          e.target.classList.contains("advantage-mobile__preview-icons")
+        ) {
+          el.classList.toggle("advantages-mobile--active");
+        }
+      });
+    });
+  }
 
-	//Quiz
-	const quiz = new Quiz();
+  //Quiz
+  const quiz = new Quiz();
 });
--- scripts/pxpe.js
+++
@@ -1,7 +0,0 @@
-import loadPixelperfect from "pixelperfect-tool";
-
-window.pixelperfect = {
-	breakpoints: [1180],
-};
-
-loadPixelperfect();
